6dda964b1c9de4853fc4f31f45983c065d5bf3fc
Alexey Panov <evil199315@yandex.ru>
2023-02-27 23:04:01 +0500

    Initial commit - created a project - added dependencies needed at the initial stage

---
+++ src/test/java/ru/panov/VotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.panov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
